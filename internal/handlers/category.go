314e019636cbfe05ad9b7059384bcea06544aa1b
Tionia R <tionia.haryono@idntimes.com>
2026-02-02 04:01:46 +0700

    use response struct

--- internal/handlers/category.go
+++ internal/handlers/category.go
@@ -27,7 +27,12 @@ func (h *CategoryHandler) HandleCategories(w http.ResponseWriter, r *http.Reques
 	case http.MethodPost:
 		h.Create(w, r)
 	default:
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		json.NewEncoder(w).Encode(models.Response{
+			Status:  "FAIL",
+			Message: "Method not allowed",
+		})
+		// http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
 }
 
@@ -39,7 +44,12 @@ func (h *CategoryHandler) HandleCategoryByID(w http.ResponseWriter, r *http.Requ
 	idStr := strings.TrimPrefix(r.URL.Path, "/api/categories/")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(models.Response{
+			Status:  "FAIL",
+			Message: "Invalid ID",
+		})
+		// http.Error(w, "Invalid ID", http.StatusBadRequest)
 		return
 	}
 
@@ -51,76 +61,144 @@ func (h *CategoryHandler) HandleCategoryByID(w http.ResponseWriter, r *http.Requ
 	case http.MethodDelete:
 		h.Delete(w, id)
 	default:
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		json.NewEncoder(w).Encode(models.Response{
+			Status:  "FAIL",
+			Message: "Method not allowed",
+		})
+		// http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
 }
 
 func (h *CategoryHandler) GetAll(w http.ResponseWriter) {
 	categories, err := h.service.GetAll()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(models.Response{
+			Status:  "FAIL",
+			Message: err.Error(),
+		})
+		// http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(categories)
+	json.NewEncoder(w).Encode(models.Response{
+		Status:  "OK",
+		Message: "Categories retrieved",
+		Data:    categories,
+	})
+	// json.NewEncoder(w).Encode(categories)
 }
 
 func (h *CategoryHandler) Create(w http.ResponseWriter, r *http.Request) {
 	var newCategory models.Category
 	err := json.NewDecoder(r.Body).Decode(&newCategory)
 	if err != nil {
-		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(models.Response{
+			Status:  "FAIL",
+			Message: "Invalid request body",
+		})
+
+		// http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
 
 	err = h.service.Create(&newCategory)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(models.Response{
+			Status:  "FAIL",
+			Message: err.Error(),
+		})
+
+		// http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(newCategory)
+	json.NewEncoder(w).Encode(models.Response{
+		Status:  "OK",
+		Message: "Category created",
+		Data:    newCategory,
+	})
+
+	// json.NewEncoder(w).Encode(newCategory)
 }
 
 func (h *CategoryHandler) GetByID(w http.ResponseWriter, id int) {
 	category, err := h.service.GetByID(id)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(models.Response{
+			Status:  "FAIL",
+			Message: err.Error(),
+		})
+
+		// http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
 
-	json.NewEncoder(w).Encode(category)
+	json.NewEncoder(w).Encode(models.Response{
+		Status:  "OK",
+		Message: "Category retrieved",
+		Data:    category,
+	})
+
+	// json.NewEncoder(w).Encode(category)
 }
 
 func (h *CategoryHandler) Update(w http.ResponseWriter, r *http.Request, id int) {
 	var categoryUpdate models.Category
 	err := json.NewDecoder(r.Body).Decode(&categoryUpdate)
 	if err != nil {
-		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(models.Response{
+			Status:  "FAIL",
+			Message: "Invalid request body",
+		})
+
+		// http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
 
 	categoryUpdate.ID = id
 	err = h.service.Update(&categoryUpdate)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(models.Response{
+			Status:  "FAIL",
+			Message: err.Error(),
+		})
+
+		// http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	json.NewEncoder(w).Encode(categoryUpdate)
+	json.NewEncoder(w).Encode(models.Response{
+		Status:  "OK",
+		Message: "Category updated",
+		Data:    categoryUpdate,
+	})
+	// json.NewEncoder(w).Encode(categoryUpdate)
 }
 
 func (h *CategoryHandler) Delete(w http.ResponseWriter, id int) {
 	err := h.service.Delete(id)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(models.Response{
+			Status:  "FAIL",
+			Message: err.Error(),
+		})
+
+		// http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	json.NewEncoder(w).Encode(map[string]string{
-		"status":  "OK",
-		"message": "Category deleted",
+	json.NewEncoder(w).Encode(models.Response{
+		Status:  "OK",
+		Message: "Category deleted",
 	})
 }
